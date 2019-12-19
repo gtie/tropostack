0736d5703da71b8ccc77c10cabd00a4e6dbd034d
tie <tropostack@morp.org>
2019-12-19 13:21:21 +0100

    Remove cruft from s3_policy test

--- tests/test_example_s3_policy.py
+++ tests/test_example_s3_policy.py
@@ -1,17 +1,7 @@
-import pytest
 from conftest import *
 
 # Stack under test
 from s3_policy import S3BucketStack as Sut
-
-#from tropostack.conf_loaders import partitioned_yaml_loader
-
-#@pytest.fixture
-#def conf(request, scope='module'):
-    #fname = os.path.join(os.path.dirname(mut.__file__), 'config_dev.yaml')
-    #with open(fname) as fhandle:
-        #result = partitioned_yaml_loader(fhandle, Sut.BASE_NAME)
-    #return result
 
 def test_stack_init():
     stack = Sut({})
