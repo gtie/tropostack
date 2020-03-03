d3e1b0e384276ddc3daa615a82c502dfd1c89a4c
tie <tropostack@morp.org>
2020-03-03 17:16:36 +0100

    Drop support for Python3.4 in setup.py (0.4.3)

--- tropostack/__init__.py
+++ tropostack/__init__.py
@@ -2,7 +2,7 @@
 CLI/workflow lib around for management of CloudFormation stacks
 """
 
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 version = __version__
 __author__ = 'tie <tropostack@morp.org>'
 __all__ = []
